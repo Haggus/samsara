f822c4eb9cd7b340acc97015d47b47c4ef677444
Mateusz 'Haggus' Mrowiec <matt.mrowiec@gmail.com>
2016-03-30 19:30:57 +0200

    Downloading and parsing rss

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+extern crate hyper;
+extern crate rss;
+extern crate chrono;
+
+use std::io::*;
+use std::ops::Sub;
+
+use hyper::Client;
+use hyper::header::Connection;
+use rss::{Rss, Channel};
+use chrono::*;
+
 fn main() {
-    println!("Hello, world!");
+    let start_time = Local::now().time();
+
+    match read_rss() {
+        Some(channel) => {
+            println!("Title:       {}", channel.title);
+            println!("Link:        {}", channel.link);
+            println!("Description: {}", channel.description);
+            println!("Items:       {}", channel.items.len());
+        }
+        None => println!("Could not read RSS"),
+    }
+
+    let total = Local::now().time().sub(start_time);
+    println!("time: {}", total);
+}
+
+fn read_rss() -> Option<Channel> {
+    let client = Client::new();
+
+    let mut res = client.get("https://www.gamingonlinux.com/article_rss.php")
+                        .header(Connection::close())
+                        .send()
+                        .unwrap();
+
+    let mut body = String::new();
+    res.read_to_string(&mut body).unwrap();
+
+    match body.parse::<Rss>() {
+        Ok(p) => Some(p.0),
+        Err(e) => {
+            println!("Error: {}", e);
+            None
+        }
+    }
 }
